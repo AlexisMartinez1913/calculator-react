d3d4da00d6f8b917f4967e2bfbeffdf79e12fd2b
Alexis Martinez <jhonyalexis100@gmail.com>
2024-08-28 17:48:13 -0500

    initialize project using vite

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-container">
+        <img
+          src=""
+          className="freecodecamp-logo"
+          alt="logo"
+        />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
